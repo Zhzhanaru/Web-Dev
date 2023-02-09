7ccf7366dac81b7865734747f15f2b6a80ec0f47
Zhanaru Manap <112745374+Zhzhanaru@users.noreply.github.com>
2023-02-09 16:21:15 +0900

    Update alert.js

--- Lab3/JavaScriptInfo/alert.js
+++ Lab3/JavaScriptInfo/alert.js
@@ -18,3 +18,12 @@ const birthday = '18.04.1982';//we can use uppercase for BIRTHDAY (for constants
 
 const age = someCode(birthday);
 
+//string quotes
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
